c872ac46c0ef5d5f70f61c891d527c0cdfd6374a
Rehan <rehanalire52@gmail.com>
2024-04-12 21:41:28 +0530

    First Commit

---
+++ MERN-Job-Portal/src/Router.jsx
@@ -0,0 +1,33 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import App from "./App";
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <h1>About</h1>,
+        },
+        {
+          path: "/contact",
+          element: <h1>Contact</h1>,
+        },
+        {
+          path: "*",
+          element: <h1>404</h1>,
+        },
+      ],
+    },
+  ]);
+
+
+  export default router
