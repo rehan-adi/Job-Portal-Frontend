b9b86bd442b48c042e0705cd2e365a5835d3dc66
Rehan <rehanalire52@gmail.com>
2024-04-15 18:36:47 +0530

    data base connected

--- MERN-Job-Portal-backend/server.js
+++ MERN-Job-Portal-backend/server.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import mongoose from 'mongoose';
 
 dotenv.config();
 const server = express();
 
+mongoose.connect(process.env.MONGODB_URI, {
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
 server.get('/', (req, res) => {
     res.send('Hello World!');
-})
+}),
 
 server.listen(process.env.PORT || 1000, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
-})
+});
