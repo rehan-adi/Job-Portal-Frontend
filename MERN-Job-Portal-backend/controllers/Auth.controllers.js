747af9de6bdffb62f5880f57d2c257e7219b42db
Rehan <rehanalire52@gmail.com>
2024-05-11 15:11:42 +0530

    some small change

--- MERN-Job-Portal-backend/controllers/Auth.controllers.js
+++ MERN-Job-Portal-backend/controllers/Auth.controllers.js
@@ -1,11 +1,17 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
-import authenticate from '../service/auth.servive.js';
 
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+    if(!name || !email || !password) {
+      return res.status(400).json({ message: 'Please fill all the fields' });
+    }
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+      return res.status(400).json({ message: 'User already exists' });
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
@@ -18,8 +24,15 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await authenticate(email, password);
-    const token = jwt.sign({ userId: user._id }, 'your_secret_key', { expiresIn: '1h' });
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new Error('Invalid credentials, Password is incorrect');
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
     console.log('Login successful');
     res.status(200).json({ token, message: 'Login successful' });
   } catch (error) {
