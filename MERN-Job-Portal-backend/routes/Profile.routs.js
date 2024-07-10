3971b85311add978c76dfcd021e55696fc1a0b92
Rehan <rehanalire52@gmail.com>
2024-07-10 16:05:49 +0530

    changes on folder structure

--- MERN-Job-Portal-backend/routes/Profile.routs.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createProfile, getProfile } from '../controllers/Profile.controllers.js';
-
-const router = express.Router();
-
-router.post('/createProfile', createProfile);
-router.get('/:id', getProfile);
-
-export default router;
