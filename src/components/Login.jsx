18312d73fe043ba39094a8e22912f3883592f534
Rehan <rehanalire52@gmail.com>
2024-07-31 00:30:34 +0530

    add Sign in with google button in Login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -78,6 +78,10 @@ const Login = () => {
               Sign In
             </button>
           </div>
+          <button className='w-full text-white rounded-md hover:bg-[#212121] duration-300 mt-12 flex justify-center items-center gap-3 font-normal text-sm bg-[#EA580C] py-2 px-5'>
+            <svg stroke="currentColor" className='mt-1' fill="currentColor" stroke-width="0" viewBox="0 0 488 512" height="25px" width="15px" xmlns="http://www.w3.org/2000/svg"><path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
+            Sign In with Google
+          </button>
           <p className='mt-8'>Don't have an account <Link to="/signup" className="text-blue-500 underline text-sm font-semibold hover:underline">Sign Up</Link> </p>
         </form>
         {error && <p className="text-red-500 text-xs italic">{error}</p>}
