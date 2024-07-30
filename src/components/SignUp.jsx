4f58c7c4c936fc5116fa7ebe378aed00133ad758
Rehan <rehanalire52@gmail.com>
2024-07-30 17:04:58 +0530

    add select tag for role in signUp page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -69,23 +69,23 @@ const SignUp = () => {
               </label>
             </div>
           </div>
-          <div className="mb-4">
-            <label htmlFor="role" className="block text-gray-200 text-sm font-semibold mb-2">
-              Role
-            </label>
+          <div className="relative h-10 w-full mt-6 min-w-[200px]">
             <select
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-[#EA580C] focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               id="role"
               value={role}
               onChange={(e) => setRole(e.target.value)}
               required
-            >
-              <option value="" disabled>Select your role</option>
-              <option value="job_seeker">Job Seeker</option>
-              <option value="employer">Employer</option>
+              >
+              <option value="">Select Your Role</option>
+              <option value="job_seeker">job_seeker</option>
+              <option value="employer">employer</option>
             </select>
+            <label
+              class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-[#EA580C] peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-[#EA580C] peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              Select a City
+            </label>
           </div>
-
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded focus:outline-none focus:shadow-outline"
