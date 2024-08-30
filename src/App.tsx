e6ca6daea8bf423e00add6dd6b30cbf20dcc70a1
Rehan <rehanalire52@gmail.com>
2024-08-30 18:42:30 +0530

    Clean up the code by remov files

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-import PostJobForm from "./components/PostJobForm.tsx";
-import JobDetail from "./components/JobDetail.tsx";
 import Login from "./components/auth/Login.tsx";
 import SignUp from "./components/auth/SignUp.tsx";
-import { isAuthenticated } from "./auth/auth.ts";
-import Profile from "./components/Profile.tsx";
-import Salary from "./components/Salary.tsx";
-import ProfileDetails from "./components/ProfileDetails.tsx";
 import Banner from "./components/Banner.tsx";
 import { Toaster } from "react-hot-toast";
 
 const App: React.FC = () => {
-  const [authenticated, setAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const authenticatedUser = isAuthenticated();
-    setAuthenticated(authenticatedUser);
-  }, []);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (authenticated && window.location.pathname === "/login") {
-      navigate("/");
-    }
-  }, [authenticated, navigate]);
-
-  const handleLogout = () => {
-    setAuthenticated(false);
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
-
   return (
     <>
       <Toaster position="top-right" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route path="/jobs/:id" element={<JobDetail />} />
           <Route index element={<Banner />} />
-          <Route path="/postjob" element={<PostJobForm />} />
-          <Route path="/createprofile" element={<Profile />} />
-          {/* <Route path="/profile/:id" element={<ProfileDetails />} /> */}
-          <Route path="/salary-estimate" element={<Salary />} />
         </Route>
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { Outlet } from 'react-router-dom';import Navbar from './Navbar.tsx';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar.tsx';
 import Footer from './Footer.tsx';
 ;
 
