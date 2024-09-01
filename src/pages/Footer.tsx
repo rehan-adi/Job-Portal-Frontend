6aaebee6c7d28b9434e51872af9be721c488a4a4
Rehan <rehanalire52@gmail.com>
2024-09-01 21:52:44 +0530

    Remove fancy things from footer and make it simple

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,54 +1,11 @@
 import React from "react";
 
-const LINKS = [
-  {
-    title: "Product",
-    items: ["Overview", "Features", "Solutions"],
-  },
-  {
-    title: "Company",
-    items: ["About us", "Careers", "News"],
-  },
-  {
-    title: "Resource",
-    items: ["Blog", "Newsletter", "Events"],
-  },
-];
-
 const currentYear = new Date().getFullYear();
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-black fixed bottom-0 pt-5 w-full">
-      <div className="mx-auto w-full max-w-7xl px-8">
-        <div className="grid grid-cols-1 justify-between items-start gap-4 md:grid-cols-2">
-          <div className="flex mb-3 lg:mb-0  items-center">
-            <img
-              src="https://github.com/mdalmamunit427/mern-job-portal-starter-files/blob/main/public/images/Linear.png?raw=true"
-              alt="Linear Logo"
-              className="h-8 mr-2"
-            />
-            <h1 className="text-white text-xl lg:text-xl font-bold">
-              HireSphere
-            </h1>
-          </div>
-          <div className="grid grid-cols-3 justify-between gap-5">
-            {LINKS.map(({ title, items }) => (
-              <ul key={title}>
-                <h2 className="mb-3 text-base font-medium text-white">
-                  {title}
-                </h2>
-                {items.map((link) => (
-                  <li key={link}>
-                    <h3 className="py-1.5 text-sm font-normal transition-colors text-white opacity-60">
-                      {link}
-                    </h3>
-                  </li>
-                ))}
-              </ul>
-            ))}
-          </div>
-        </div>
+      <div className="mx-auto w-full max-w-7xl px-2">
         <div className="mt-12 flex w-full text-white flex-col items-center justify-center border-t border-white border-opacity-15 py-4 md:flex-row md:justify-between">
           <p className="mb-4 text-center font-normal text-sm md:mb-0">
             &copy; {currentYear}{" "}
@@ -104,20 +61,6 @@ const Footer: React.FC = () => {
                 <path
                   fill-rule="evenodd"
                   d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span>
-            <span className="opacity-80 transition-opacity hover:opacity-100">
-              <svg
-                className="h-5 w-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
                   clip-rule="evenodd"
                 />
               </svg>
