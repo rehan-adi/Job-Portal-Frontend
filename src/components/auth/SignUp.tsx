539d1718dca57cebe514d824c3111d04c5596f22
Rehan <rehanalire52@gmail.com>
2024-09-01 17:38:51 +0530

    Change button variant and size in Signup page and make change on select component

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -110,9 +110,10 @@ const SignUp: React.FC = (): JSX.Element => {
             />
             <Button
               type="submit"
-              variant="outline"
+              variant="default"
               className="w-full"
               disabled={loading}
+              size="lg"
             >
               {loading ? (
                 <>
@@ -125,13 +126,17 @@ const SignUp: React.FC = (): JSX.Element => {
             </Button>
           </form>
         </Form>
-        <button
-          type="button"
-          className="w-full text-white rounded-md hover:bg-[#212121] duration-300 mt-12 flex justify-center items-center gap-3 font-normal text-sm bg-[#EA580C] py-2 px-5"
-        >
-          <svg
+        <div className="mt-4">
+        <Button
+              type="submit"
+              variant="outline"
+              className="w-full"
+              disabled={loading}
+              size="lg"
+            >
+               <svg
             stroke="currentColor"
-            className="mt-1"
+            className="mt-1 mr-3"
             fill="currentColor"
             strokeWidth="0"
             viewBox="0 0 488 512"
@@ -142,12 +147,13 @@ const SignUp: React.FC = (): JSX.Element => {
             <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
           </svg>
           Sign Up with Google
-        </button>
+        </Button>
+        </div>
         <p className="mt-6 text-center text-white">
           Already have an account?{" "}
           <Link
             to="/signin"
-            className="text-[#EA580C] underline text-sm font-semibold hover:underline"
+            className="text-[#F97316] underline text-sm font-semibold hover:underline"
           >
             Sign In
           </Link>
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -22,7 +22,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-9 w-full items-center justify-between dark whitespace-nowrap rounded-md border border-opacity-15 border-input px-3 py-3 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-9 w-full items-center justify-between dark whitespace-nowrap rounded-md border border-opacity-15 border-input px-3 py-5 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
