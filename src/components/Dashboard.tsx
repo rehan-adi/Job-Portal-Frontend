364f87fc4dcb9ad30e189cad26de7bc753921312
Rehan <rehanalire52@gmail.com>
2024-09-12 00:16:01 +0530

    Fix a small mistake in Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "@/components/Navbar.tsx"
 import Footer from "@/components/Footer.tsx"
-import Navbar from "@/components/Footer.tsx"
 
 const Dashboard = () => {
   return (
